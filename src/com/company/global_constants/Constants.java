3c1ef197a42598a73244e7f80d5fe0329b30b0e9
DinkoBahchevanov <dinko_bahchevanov@abv.bg>
2021-02-02 09:45:26 +0200

    add busted and win constants

--- src/com/company/global_constants/Constants.java
+++ src/com/company/global_constants/Constants.java
@@ -6,4 +6,6 @@ public class Constants {
     public static final String RED_GPS    = "RED_GPS";
     public static final String YELLOW_GPS = "YELLOW_GPS";
     public static final String QUESTION = "QUESTION";
+    public static final String BUSTED = "BUSTED!!1! NO EXIT";
+    public static final String WIN = "CONGRATS! YOU FOUND BABA QGA's HOUSE";
 }
